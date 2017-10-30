f8d71deb8f8294db4fa06b09127b1c07e78bd5ea
maryli891 <marytta095@gmail.com>
2017-10-30 19:50:39 -0300

    se realizo el Ejercicio AreaRectangulo

--- src/pruebajava1/AreaRectangulo.java
+++ src/pruebajava1/AreaRectangulo.java
@@ -4,11 +4,27 @@
  * and open the template in the editor.
  */
 package pruebajava1;
-
-/**
- *
+import java.util.Scanner;
+/********************************************************************
+ * Fecha 30 de Octubre 2017
  * @author LC22
- */
-public class AreaRectangulo {
+ * Clase PruebaJava1
+ * Responsabilidad Clase Principal
+ ********************************************************************/
+public class AreaRectangulo 
+{
+public static void main(String[] args)
+{  
+    int area;
     
-}
+    
+   Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese la base del rectangulo");
+           int base =sc.nextInt();          
+        System.out.println("Ingrese la altura del rectangulo");
+        int altura =sc.nextInt(); 
+        area=base*altura;
+        System.out.println(" El area del rectangulo es: "+area+" mts2");                
+     }
+} 
+
