3a44d8e43edd1d02f8f83db3d2f84fce44d7da70
maryli891 <marytta095@gmail.com>
2017-10-30 18:46:52 -0300

    Se ha creado el encabezado multimedia

--- src/pruebajava1/PruebaJava1.java
+++ src/pruebajava1/PruebaJava1.java
@@ -6,8 +6,11 @@
 package pruebajava1;
 
 /**
- *
+ * Mariane Muñoz Gonzalez
+ *Fecha 30 de Octubre 2017
  * @author LC22
+ * Clase PruebaJava1
+ * Responsabilidad Clase Principal
  */
 public class PruebaJava1 {
 
